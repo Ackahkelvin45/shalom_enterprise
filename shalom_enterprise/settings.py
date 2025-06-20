0eeda1cfa6c46d33d3f841977b2a7174521ab4c6
ackahkelvin45 <ackahkelvin455@gmail.com>
2025-06-20 17:59:13 +0000

    changes

--- shalom_enterprise/settings.py
+++ shalom_enterprise/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-06sxf-rs^o_kplj1k-m@2ji%9f#8d45*usxhxdnl3s4%1di@2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['65.227.85.34','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['165.227.85.34','localhost','127.0.0.1']
 
 
 # Applicatio  "http://165.227.85.34",
