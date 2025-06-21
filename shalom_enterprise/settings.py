eac3f944cd10709a96862fd6e5f55d7ee48f42ce
ackahkelvin45 <ackahkelvin455@gmail.com>
2025-06-21 10:02:15 +0000

    changes

--- shalom_enterprise/settings.py
+++ shalom_enterprise/settings.py
@@ -158,10 +158,12 @@ AUTH_USER_MODEL = "authentication.CustomUser"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv("EMAIL")  # Make sure this matches exactly
 EMAIL_HOST_PASSWORD =  os.getenv("EMAIL_PASSWORD")
+DEFAULT_FROM_EMAIL="enterpriseshalom676@gmail.com"
 
 
 
