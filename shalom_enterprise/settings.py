a9b20f43e3ae6f0062de5a51e1a256753b56e729
ackahkelvin45 <ackahkelvin455@gmail.com>
2025-06-21 11:13:30 +0000

    changes

--- shalom_enterprise/settings.py
+++ shalom_enterprise/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-06sxf-rs^o_kplj1k-m@2ji%9f#8d45*usxhxdnl3s4%1di@2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['165.227.85.34','localhost','127.0.0.1','www.google.com']
+ALLOWED_HOSTS = ['165.227.85.34','localhost','127.0.0.1','www.google.com','wwww.saexpress.shop','saexpress.shop']
 
 
 # Applicatio  "http://165.227.85.34",
@@ -186,7 +186,8 @@ CORS_ALLOWED_ORIGINS = [
     'shalom-enterprise-1.onrender.com',
       "http://165.227.85.34",
     "https://165.227.85.34",
-     'https://www.google.com'
+     'https://www.google.com',
+        'https://saexpress.shop'
     ]
 
 CORS_ALLOW_CREDENTIALS = True  # To allow cookies
@@ -202,6 +203,7 @@ CSRF_TRUSTED_ORIGINS = [
       "http://165.227.85.34",
     "https://165.227.85.34",
     'https://www.google.com'
+    'https://saexpress.shop'
     
 
 ]
